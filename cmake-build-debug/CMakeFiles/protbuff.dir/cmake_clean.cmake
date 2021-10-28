8893091d47f0af00566012c5c5b2a647babb6256
meresse kyalwe <meressekyalwe@live.com>
2021-10-28 18:00:11 +0300

    commit#1

---
+++ cmake-build-debug/CMakeFiles/protbuff.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/protbuff.dir/main.cpp.obj"
+  "libprotbuff.dll.a"
+  "protbuff.exe"
+  "protbuff.exe.manifest"
+  "protbuff.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/protbuff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
